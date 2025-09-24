6760b6e8132517420c55c2802a676195423236e7
Farg0n <miguelanga1604@gmail.com>
2025-09-24 06:30:49 -0500

    Initial commit

---
+++ POO-Practice/Models/Person.cs
@@ -0,0 +1,18 @@
+namespace POO_Practice.Models;
+
+// Se crea la clase padre
+public class Person
+{
+    //Se crean los atrubutos de la clase 
+    protected string Name { get; set; }
+    protected string Gender { get; set; }
+    protected int Age {  get; set; }
+    protected int Weight { get; set; }
+    protected double Height { get; set; }
+
+    public Person(string Name, string Gender,)
+    {
+        
+    }
+    
+}
