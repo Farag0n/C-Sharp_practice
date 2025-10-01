9c59323b2e069a141e1b4450c3ff6a8f39d6bb98
Faragon <111461692+Farag0n@users.noreply.github.com>
2025-10-01 15:08:02 -0500

    Delete TallerLinQ directory

--- TallerLinQ/Models/Course.cs
+++
@@ -1,9 +0,0 @@
-namespace TallerLinQ.Models;
-
-public class Course
-{
-    public int id { get; set; }
-    public string title { get; set; }
-    public int credits { get; set; }
-//    public virtual Student Student { get; set; }
-}
--- TallerLinQ/Models/Enrollment.cs
+++
@@ -1,9 +0,0 @@
-namespace TallerLinQ.Models;
-
-public class Enrollment
-{
-    public int id { get; set; }
-    public int StudentId { get; set; }
-    public int CourseId { get; set; }
-    public double grade { get; set; }
-};
